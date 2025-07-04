0e5d035fc4a592a6b4ed3c5093583593c6035813
Mohammad Ali <always.mohammad.ali@gmail.com>
2025-07-04 18:31:17 +0600

    user profile & verified logo done

--- js/video.js
+++ js/video.js
@@ -41,18 +41,29 @@ const displayVideos = (videoData) => {
     const cardVideo = document.createElement("div")
     cardVideo.classList = 'card bg-base-100 w-96 shadow-sm'
     cardVideo.innerHTML = `
-             <figure class="px-10 pt-10">
+             <figure class="px-10 pt-10 h-[200px]">
  <img
       src=${data.thumbnail}
       alt="Shoes"
-      class="rounded-xl" />
+      class="rounded-xl h-full w-full object-cover" />
   </figure>
-  <div class="card-body items-center text-center">
-    <h2 class="card-title">Card Title</h2>
-    <p>A card component has a figure, a body part, and inside body there are title and actions parts</p>
-    <div class="card-actions">
-      <button class="btn btn-primary">Buy Now</button>
-    </div>
+  <div class="w-96  flex gap-3 justify-start  my-3">
+    
+      <div>
+         <img class="h-10 w-10 rounded-full" src="${data.authors[0].profile_picture}"/>
+      </div>
+
+      <div>
+         <h2 class="text-xl font-bold">${data.title}</h2>
+
+         <div class="flex gap-2 items-center">
+            <p class="text-gray-400">${data.authors[0].profile_name}</p>
+            <img class="w-5 h-5" src="https://img.icons8.com/?size=100&id=D9RtvkuOe31p&format=png&color=000000"/>
+         </div>
+         
+         
+      </div>
+    
   </div>
         `
     videoContainer.append(cardVideo)
