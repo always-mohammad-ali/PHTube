7ab0b2707c273627b406d5d5e7a805e9fd2e703e
Mohammad Ali <always.mohammad.ali@gmail.com>
2025-07-06 19:29:46 +0600

    modal add to show user the description of a video

--- js/video.js
+++ js/video.js
@@ -1,27 +1,34 @@
-// create load categories
+// CREATE LOAD FUNCTION TO GET THE CATEGORY DATA BY FETCHING API
 const load = () => {
   fetch("https://openapi.programming-hero.com/api/phero-tube/categories")
     .then((res) => res.json())
+    // DISPLAY FUNCTION CALLED HERE TO GET THE DATA INSIDE ITS PARAMETER BY 'DATA' NAME
     .then((data) => display(data.categories))
     .catch((error) => console.log(error));
 };
 
+// CALL LOAD FUNCTION
 load();
-// create display
+
+// CREATE DISPLAY FUNCTION FOR GETTING 3 CATEGORY ITEMS BTN IN THE UI
 const display = (data) => {
   const catagoriesContainer = document.getElementById("catagories-container");
   data.forEach((element) => {
     console.log(element);
     const btnContainer = document.createElement("div")
+
+    // CATEGORYVIDEOLOAD FUNCTION CALL HERE AS ONCLICK WHEN BUTTON CLICKED, THEN IT WILL FETCHING API DATA TO HERE AND SHOW ALL THE VIDEOS
     btnContainer.innerHTML = `
+    
         <button id="btn-${element.category_id}" onclick = "categoryVideoLoad(${element.category_id})" class="btn btn-style">${element.category}</button>
     `
 
-    
+    // BTNCONTAINER APPEND BY CATAGORIESCONTAINER
     catagoriesContainer.append(btnContainer);
   });
 };
 
+// CATEGORYVIDEOLOAD FUNCTION LOADS CATEGORIZE VIDEO FROM API
 const categoryVideoLoad = (id) =>{
     // alert(id)
 
@@ -29,6 +36,7 @@ const categoryVideoLoad = (id) =>{
     .then((res) => res.json())
     .then((data) =>
       { 
+        // REMOVEACTIVECLASS FUNCTION CALLS HERE TO REMOVE ACTIVE CLASS FIRST THEN AFTER THIS FUNCTION ACTIVE CLASS WILL CREATE AGAIN IN THE FOLLOWING CODE
         removeActiveClass();
 
         const activeBtn = document.getElementById(`btn-${id}`);
@@ -37,6 +45,7 @@ const categoryVideoLoad = (id) =>{
     .catch((err) => console.error(err));
 }
 
+// REMOVEACTIVECLASS FUNCTION USE HERE TO REMOVE ACTIVE CLASS
 const removeActiveClass =()=>{
         const btnStyle = document.getElementsByClassName('btn-style');
         for(let buttonStyle of btnStyle){
@@ -44,6 +53,7 @@ const removeActiveClass =()=>{
         }
 }
 
+// LOADVIDEOS FUNCTION LOADS ALL VIDEOS AND THEN IT SENDS ALL VIDEO DATA TO DISPLAYVIDEOS FUNCTION PARAMETER BY CALLING THIS INSIDE DATA
 const loadVideos = () => {
   fetch("https://openapi.programming-hero.com/api/phero-tube/videos")
     .then((res) => res.json())
@@ -53,6 +63,7 @@ const loadVideos = () => {
 
 loadVideos();
 
+// DISPLAYVIDEOS FUNCTION WILL TAKE DATA FROM LOADVIDEOS AND THEN USE IT INSIDE ITS VIDEOCONTAINER SECTION.
 const displayVideos = (videoData) => {
   const videoContainer = document.getElementById("videos");
   videoContainer.innerHTML = "";
@@ -116,7 +127,8 @@ const displayVideos = (videoData) => {
             }
          </div>
          
-         
+         <p> <button class="btn btn-sm btn-error" onclick="loadVideoDetails('${data.video_id}')"> Details </button> </p>
+
       </div>
     
   </div>
@@ -125,6 +137,31 @@ const displayVideos = (videoData) => {
   });
 };
 
+// LOADVIDEODETAILS FUNCTION IS USED FOR SHOWING DETAILS OF A INDIVIDUAL VIDEO BY CLICKING THE BUTTON 
+const loadVideoDetails = async(videoID) =>{
+  const uri = `https://openapi.programming-hero.com/api/phero-tube/video/${videoID}`
+  const res = await fetch(uri)
+  const data = await res.json()
+  displayVideoDetails(data.video)
+}
+
+const displayVideoDetails = (details) =>{
+    console.log(details)
+    const modalContent = document.getElementById('modal-content')
+
+    modalContent.innerHTML = `
+    <img src="${details.thumbnail}"/>
+    <p>${details.description}</p>
+    `
+    
+    // WAY-1 TO SHOW MODAL DATA
+    document.getElementById('customModal').showModal()
+
+    // WAY-2 TO SHOW MODAL DATA
+    // document.getElementById('showModalData').click()
+}
+
+// CALCULATETIME FUNCTION CALCULATE SECONDS TO HRS, MIN & REMAINING SECONDS
 function calculateTime(time) {
    
     let hours = parseInt(time / 3600);
